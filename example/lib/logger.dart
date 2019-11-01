9c70c259705d36e5e58c5744622b2a4388c4449d
synw <ansterj@gmail.com>
2019-11-01 23:23:31 +0100

    Update example

--- example/lib/logger.dart
+++ example/lib/logger.dart
@@ -1,9 +1,3 @@
 import 'package:err/err.dart';
 
-var logger = ErrRouter(
-    criticalRoute: [ErrRoute.console, ErrRoute.screen],
-    errorRoute: [ErrRoute.screen, ErrRoute.console],
-    warningRoute: [ErrRoute.screen, ErrRoute.console],
-    infoRoute: [ErrRoute.screen, ErrRoute.console],
-    debugRoute: [ErrRoute.screen, ErrRoute.console],
-    deviceConsole: true);
+var logger = ErrRouter(channel: ErrChannel.dev, deviceConsole: true);
