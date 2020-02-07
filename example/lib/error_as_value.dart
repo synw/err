5626991382b4c49c9b3e33d26cbc9ae88e5b097a
synw <synw@yahoo.com>
2020-02-07 08:51:32 +0100

    Add comments to example

--- example/lib/error_as_value.dart
+++ example/lib/error_as_value.dart
@@ -4,17 +4,21 @@ ErrPack<int> _someFunctionThatReturnsAnInt() {
   try {
     throw Exception("Oops");
   } catch (e) {
+    // return an error and no value
     return ErrPack.err(Err.warning(e));
   }
+  // return no error and a integer value
   return const ErrPack.ok(1);
 }
 
-ErrPack _someFunctionThatReturnsNull() {
+ErrPack<Null> _someFunctionThatReturnsNull() {
   try {
     // ok
   } catch (e) {
-    return ErrPack.err(Err.debug(e));
+    // return an error and no value
+    return ErrPack.err(Err.debug("Something went wrong"));
   }
+  // return no error and a null value
   return const ErrPack.nullOk();
 }
 
